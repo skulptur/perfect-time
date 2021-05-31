c7076402828eabdfb2666424501c625b9838fea4
Gustavo Spredemann <gustavo.spredemann@mediamonks.com>
2021-05-31 16:46:08 -0300

    Update index.tsx

--- example/index.tsx
+++ example/index.tsx
@@ -1,20 +1,25 @@
 import { manual } from './src/manual'
+import { timeStretch } from './src/timeStretch'
 import { interval } from './src/interval'
 import { scriptProcessor } from './src/scriptProcessor'
 import { resumeContext } from 'audio-fns'
 
 const example = {
+  timeStretch,
   manual,
   interval,
   scriptProcessor,
 }
 
 resumeContext(new AudioContext()).then((context) => {
+  // console.log('- timeStretch')
+  example.timeStretch(context)
+
   // console.log('- manual')
   // example.manual()
 
-  console.log('- interval')
-  example.interval(context)
+  // console.log('- interval')
+  // example.interval(context)
 
   // console.log('- scriptProcessor')
   // example.scriptProcessor(context)
